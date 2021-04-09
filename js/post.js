08e69e37a5796608b251e5ce4cf00522e26768d3
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-04-09 11:28:59 +0300

    updated urlParams for single post

--- js/post.js
+++ js/post.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get('post');
 
-fetch(`https://spring21-427e.restdb.io/rest/posts/${postId}`, {
+fetch(`https://spring21-427e.restdb.io/rest/posts/${postId}?fetchchildren=true`, {
   "method": "GET",
   "headers": {
     "x-apikey": "6034a7eb5ad3610fb5bb6548"
@@ -18,4 +18,6 @@ fetch(`https://spring21-427e.restdb.io/rest/posts/${postId}`, {
 function showPost(data) {
   console.log(data);
   document.querySelector('.top>h1').textContent = data.title;
+  document.querySelector('.username').textContent = data.username;
+  document.querySelector('.post-content').textContent = data.content;
 }
