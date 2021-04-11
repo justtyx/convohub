38655dca0d95f9bf244da362ef098c8d75a084fb
Justina Jemeljanovaite <just1528@stud.kea.dk>
2021-04-11 14:05:30 +0300

    working database w/ live comments

--- js/post.js
+++ js/post.js
@@ -20,4 +20,75 @@ function showPost(data) {
   document.querySelector('.top>h1').textContent = data.title;
   document.querySelector('.username').textContent = data.username;
   document.querySelector('.post-content').textContent = data.content;
+
+  //comment template
+  const template = document.querySelector('.comment-template').content;
+
+  data.comments.forEach(comment => {
+  //clone
+  const clone = template.cloneNode(true);
+
+  //loop through comments
+
+    console.log(comment);
+    clone.querySelector('.comm-user').textContent = comment.username;
+    clone.querySelector('.comm-user').textContent = comment.username;
+    clone.querySelector('.comment-content').textContent = comment.content;
+
+    const parent = document.querySelector('section.comment-section');
+    parent.appendChild(clone);
+  });
+
+  if (data.comments.length === 0) {
+    // document.querySelector('section.comment-section').classList.add('hide');
+    document.querySelector('.no-comments').classList.remove('hide');
+  }
+
+}
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', userSubmitted);
+
+function userSubmitted(e) {
+  e.preventDefault();
+
+  const payload = {
+  username: form.elements.username.value,
+  email: form.elements.email.value,
+  content: form.elements.content.value,
+  }
+    
+  document.querySelector('button[type="submit"]').disabled = true;
+
+  fetch(`https://spring21-427e.restdb.io/rest/posts/${postId}/comments`, {
+  "method": "POST",
+  "headers": {
+  "x-apikey": "6034a7eb5ad3610fb5bb6548",
+  "Content-Type": "application/json"
+  },
+  "body": JSON.stringify(payload),
+  })
+  
+  .then ((res) => res.json())
+  .then((response) => {
+    console.log(response);
+    document.querySelector('button[type="submit"]').disabled = false;
+    form.elements.username.value = '';
+    form.elements.email.value = '';
+    form.elements.content.value = '';
+
+    const template = document.querySelector('.comment-template').content;
+    const clone = template.cloneNode(true);
+    clone.querySelector('.comm-user').textContent = response.username;
+    clone.querySelector('.comm-user').textContent = response.username;
+    clone.querySelector('.comment-content').textContent = response.content;
+
+    const parent = document.querySelector('section.comment-section');
+    parent.appendChild(clone);
+
+  })
+  .catch(err => {
+    console.error(err);
+  });
 }
